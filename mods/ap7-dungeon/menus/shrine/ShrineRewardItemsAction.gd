e05b0c4e3e895c332131ba95fae5f36544c823c8
Apjjm <Apjjm@users.noreply.github.com>
2024-02-03 21:04:25 +0000

    (b6) fix item shrine rewards

--- mods/ap7-dungeon/menus/shrine/ShrineRewardItemsAction.gd
+++ mods/ap7-dungeon/menus/shrine/ShrineRewardItemsAction.gd
@@ -5,12 +5,14 @@ export(int) var quantity = -1
 
 func _run():
 	var to_give = []
-	if quantity < 0:
-		to_give.append_array(self.items)
+	if quantity <= 0:
+		for item in self.items:
+			to_give.append_array([{ "item": item, "amount": 1 }])
 	else:
 		var rng = get_rng()
-		for _i in range(rng.rand_int(quantity)):
-			to_give.push_back(rng.choice(self.items))
-
+		for _i in range(quantity):
+			var item = ItemFactory.generate_item(rng.choice(self.items), rng)
+			to_give.append_array([{ "item": item, "amount": 1 }])
+	
 	MenuHelper.give_items(to_give)
 	return true
